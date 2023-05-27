fd0f5db4df44b21222c4bac702e00414986ce92f
Prateek Singh <iamprateek22@gmail.com>
2023-05-27 15:25:18 +0530

    Update settings.py

--- discuso/settings.py
+++ discuso/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app",".now.sh","127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app","127.0.0.1"]
 
 
 # Application definition
