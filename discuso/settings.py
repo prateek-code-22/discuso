d5e480eb0484543fc1693f03aabc3029affe70c7
prateek <iamprateek22@gmail.com>
2025-08-04 13:18:07 +0530

    updated vercel.json

--- discuso/settings.py
+++ discuso/settings.py
@@ -149,7 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'  
 # Path where media is stored  
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 
 
 # Default primary key field type
